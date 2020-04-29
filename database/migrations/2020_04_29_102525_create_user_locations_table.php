06211e5dca8321331ee2f1861928ec8b8b58b88d
mbaliya26 <mbaliya26@gmail.com>
2020-04-29 16:02:24 +0530

    intinliaze

---
+++ database/migrations/2020_04_29_102525_create_user_locations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_locations', function (Blueprint $table) {
+            $table->id();
+            $table->json('coors')->nullable();
+            $table->string('key')->nullable();
+            $table->timestamp('timestamp')->nullable();
+            $table->string('user_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_locations');
+    }
+}
